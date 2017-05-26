93a280d922ab9783d5a0ec14c26864249ce8ec5d
kld <kldgarrido.com>
2017-05-26 05:59:08 -0500

    Crea la ruta PATCH /products/:id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,15 @@ class ProductsController < ApplicationController
 	  end
 	end
 
+	def update
+	  @product = Product.find(params[:id])
+	  if @product.update(product_params)
+	    redirect_to products_path, notice: "El producto ha sido modificado con éxito"
+	  else
+	    render :edit
+	  end
+	end
+
 	private
 	  def product_params
 	    params.require(:product).permit(:name, :url, :description)
