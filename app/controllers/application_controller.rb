aac2c830c0e5fb69436c42671fd1be0f3f45bda5
kld <kldgarrido.com>
2017-05-28 12:22:12 -0500

    Restringe las páginas privadas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  	protect_from_forgery with: :exception
+
+  	
 
 
   	def sign_in(user)
@@ -19,14 +21,25 @@ class ApplicationController < ActionController::Base
 	  def signed_in?
 	    !current_user.nil?
 	  end
-	  
 	  helper_method :signed_in?
+
 
 	  def current_user
 	    @current_user ||= User.find(cookies.signed[:user_id]) if cookies.signed[:user_id]
 	  	rescue ActiveRecord::RecordNotFound
 	  end
-	  
 	  helper_method :current_user
+
+
+	  def private_access
+  		redirect_to :login unless signed_in?
+	  end
+
+
+	  def public_access
+  		redirect_to root_path if signed_in?
+	  end
+
+
 
 end
