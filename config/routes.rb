8b6c80390ecb5614218ab43d19ecb7d2a9180cf5
kld <kldgarrido.com>
2017-05-28 10:30:07 -0500

    Implementa la funcionalidad de autenticarse

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root 'products#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
   resources :users, only: [:new, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products
   
-  root 'products#index'
   
 end
