394ac6197a2446ba524e54635e7db7d62a1f9e49
kld <kldgarrido.com>
2017-06-07 13:09:48 -0500

    Implementa las imágenes para los productos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,7 +46,7 @@ class ProductsController < ApplicationController
 
 	private
 	  def product_params
-	    params.require(:product).permit(:name, :url, :description)
+	    params.require(:product).permit(:name, :url, :description, :image)
 	  end
 
 end
